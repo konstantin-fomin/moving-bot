4c1d1c617cacdec128b96d195014845eda8041c0
Konstantin Fomin <parmezzzan@gmail.com>
2026-07-06 06:11:02 +0000

    Export checklist database types

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,19 +1,23 @@
 from app.database.storage import (
     ActionResult,
+    Category,
     ChecklistItem,
     Database,
     Invitation,
     NewChecklistItem,
     Role,
+    Status,
     User,
 )
 
 __all__ = [
     "ActionResult",
+    "Category",
     "ChecklistItem",
     "Database",
     "Invitation",
     "NewChecklistItem",
     "Role",
+    "Status",
     "User",
 ]
